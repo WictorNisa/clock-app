1459d0df6e7cfaecdfdfd6dc0bd1891ae131e414
WictorNisa <wictornisa@hotmail.com>
2023-11-22 17:59:36 +0100

    Added timezone functionality

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const timeLocation = document.getElementById("timezoneLocation");
 const dayOfYear = document.getElementById("dayOfYear");
 const dayOfWeek = document.getElementById("dayOfWeek");
 const weekNumber = document.getElementById("weekNumber");
+const greeting = document.getElementById("greeting");
+const heroImage = document.getElementById("heroContainer");
 
 document.addEventListener("DOMContentLoaded", function () {
   // Hide information div on page load
@@ -15,6 +17,24 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const url = "https://api.quotable.io/random";
+
+// Create a timeline
+const quoteTimeline = gsap.timeline({ paused: true });
+
+// Add animations to the timeline
+quoteTimeline
+  .fromTo(quote, { opacity: 0 }, { opacity: 1, duration: 0.8 })
+  .fromTo(author, { opacity: 0 }, { opacity: 1, duration: 0.8 }, "-=0.2"); // Use "-=0.2" to start the author animation 0.2 seconds before the quote animation ends
+
+// Function to play the animation
+function playQuoteAnimation() {
+  quoteTimeline.restart();
+}
+
+// Function to reverse the animation
+// function reverseQuoteAnimation() {
+//   quoteTimeline.reverse();
+// }
 
 function getRandomQuote() {
   fetch(url)
@@ -25,8 +45,9 @@ function getRandomQuote() {
       return response.json();
     })
     .then((quoteData) => {
-      quote.innerText = `" ${quoteData.content} "`;
-      author.innerText = quoteData.author;
+        quote.innerText = `" ${quoteData.content} "`;
+        author.innerText = `- ${quoteData.author}`;
+      playQuoteAnimation();
     })
     .catch((error) => {
       console.error(`Error: ${error.message}`);
@@ -49,6 +70,19 @@ function getUserLocation() {
       const hours = dataTime.getHours();
       const minutes = dataTime.getMinutes();
       //Display hours and minutes
+      if (hours < 12) {
+        greeting.innerText = "morning";
+        heroImage.style.backgroundImage =
+          "url('img/luca-bravo-ESkw2ayO2As-unsplash.jpg')";
+        console.log("morning background");
+        heroImage.style.color = "black";
+      } else if (hours < 18) {
+        greeting.innerText = "afternoon";
+        heroImage.style.backgroundImage =
+          "url('img/joshua-woroniecki-0289jpHHk0o-unsplash.jpg')";
+        console.log("afternoon background");
+        heroImage.style.color = "white";
+      }
 
       time.innerText = `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
       timeZone.innerText = locationData.abbreviation;
@@ -80,5 +114,9 @@ document.getElementById("toggleButton").addEventListener("click", function () {
   // Toggle the visibility of the information section
   informationDiv.classList.toggle("hidden");
 });
-// getRandomQuote();
+
+getRandomQuote();
 getUserLocation();
+setInterval(getRandomQuote, 10000);
+setInterval(getUserLocation, 60000);
+
